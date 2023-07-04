6a6e9c8a78adddc356c1568be4939107575ec8af
Amitesh <rakshitamitesh@gmail.com>
2023-07-04 12:17:27 +0530

    done with the portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,60 +7,73 @@ import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.jpg'
 import IMG6 from '../../assets/portfolio6.jpg'
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Something',
+    github: 'https://github.com/Amitesh100',
+    demo: ''
+  },
+  {
+    id: 1,
+    image: IMG2,
+    title: 'Something',
+    github: '',
+    demo: ''
+  },
+  {
+    id: 1,
+    image: IMG3,
+    title: 'Something',
+    github: '',
+    demo: ''
+  },
+  {
+    id: 1,
+    image: IMG4,
+    title: 'Something',
+    github: '',
+    demo: ''
+  },
+  {
+    id: 1,
+    image: IMG5,
+    title: 'Something',
+    github: '',
+    demo: ''
+  },
+  {
+    id: 1,
+    image: IMG6,
+    title: 'Something',
+    github: '',
+    demo: ''
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a className='btn' href="https://github.com">Github</a>
-          <a className='btn btn-primary' href="https://github.com">Live Demo</a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a className='btn' href="https://github.com">Github</a>
-          <a className='btn btn-primary' href="https://github.com">Live Demo</a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a className='btn' href="https://github.com">Github</a>
-          <a className='btn btn-primary' href="https://github.com">Live Demo</a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a className='btn' href="https://github.com">Github</a>
-          <a className='btn btn-primary' href="https://github.com">Live Demo</a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a className='btn' href="https://github.com">Github</a>
-          <a className='btn btn-primary' href="https://github.com">Live Demo</a>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <a className='btn' href="https://github.com">Github</a>
-          <a className='btn btn-primary' href="https://github.com">Live Demo</a>
-        </article>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a className='btn' href={github}>Github</a>
+                  <a className='btn btn-primary' href={demo} target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
