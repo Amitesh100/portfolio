ce38d9596baf18955961e4599b1a3cf7d0d2394f
Amitesh <rakshitamitesh@gmail.com>
2023-07-04 11:21:46 +0530

    Done with the services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,87 @@
 import React from "react";
 import './services.css'
+import { BiCheck } from 'react-icons/bi'
 
 const Services = () => {
    return (
       <section id="services">
-         Services
+         <h5>What I Offer</h5>
+         <h2>Services</h2>
+
+         <div className="container services__container">
+            <article className="service">
+               <div className="service__head">
+                  <h3>UI/UX Design</h3>
+               </div>
+               <ul className="service__list">
+                  <li>
+                     <BiCheck className="service__list-icon" />
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, numquam.</p>
+                  </li>
+                  <li>
+                     <BiCheck className="service__list-icon" />
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, numquam.</p>
+                  </li>
+                  <li>
+                     <BiCheck className="service__list-icon" />
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, numquam.</p>
+                  </li>
+                  <li>
+                     <BiCheck className="service__list-icon" />
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, numquam.</p>
+                  </li>
+               </ul>
+            </article>
+            {/* END OF UI/UX DESIGN SERVICE */}
+            <article className="service">
+               <div className="service__head">
+                  <h3>Web Development</h3>
+               </div>
+               <ul className="service__list">
+                  <li>
+                     <BiCheck className="service__list-icon" />
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, numquam.</p>
+                  </li>
+                  <li>
+                     <BiCheck className="service__list-icon" />
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, numquam.</p>
+                  </li>
+                  <li>
+                     <BiCheck className="service__list-icon" />
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, numquam.</p>
+                  </li>
+                  <li>
+                     <BiCheck className="service__list-icon" />
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, numquam.</p>
+                  </li>
+               </ul>
+            </article>
+            {/* END OF WEB DEVELOPMENT SERVICE */}
+            <article className="service">
+               <div className="service__head">
+                  <h3>Content Creation</h3>
+               </div>
+               <ul className="service__list">
+                  <li>
+                     <BiCheck className="service__list-icon" />
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, numquam.</p>
+                  </li>
+                  <li>
+                     <BiCheck className="service__list-icon" />
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, numquam.</p>
+                  </li>
+                  <li>
+                     <BiCheck className="service__list-icon" />
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, numquam.</p>
+                  </li>
+                  <li>
+                     <BiCheck className="service__list-icon" />
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, numquam.</p>
+                  </li>
+               </ul>
+            </article>
+            {/* END OF CONTENT CREATION SERVICE */}
+         </div>
       </section>
    )
 }
