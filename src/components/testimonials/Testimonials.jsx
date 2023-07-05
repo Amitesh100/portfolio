28c53d79a7fbdc92bebeaaa863734bd897a6f2eb
Amitesh <rakshitamitesh@gmail.com>
2023-07-05 20:37:42 +0530

    add swipe functionality to the testimonials using SwiperJS

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,14 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+// SwiperJS imports
+import { Pagination } from 'swiper/modules'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+
 const data = [
   {
     avatar: AVTR1,
@@ -34,21 +42,27 @@ const Testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
       
-      <div className="container testimonials__container">
+      <Swiper className="container testimonials__container"
+            // install Swiper modules
+            modules={[Pagination]}
+            spaceBetween={40}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+      >
         {
           data.map(({avatar, name, review}, index) => {
             return (
-              <article className="testimonial">
+              <SwiperSlide className="testimonial">
               <div className="client__avatar">
                 <img src={avatar} alt={name} />
               </div>
               <h5 className="client__name">{name}</h5>
               <small className="client__review">{review}</small>
-            </article>
+            </SwiperSlide>
             )
           })
         }
-      </div>
+      </Swiper>
     </section>
   )
 }
